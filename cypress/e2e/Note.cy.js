fceb76d9d3d61fc673eda5f0d7611e2b732c3aa3
Dale Case <dale@smartlogic.io>
2025-07-22 11:42:31 -0400

    added to note test

--- cypress/e2e/Note.cy.js
+++ cypress/e2e/Note.cy.js
@@ -4,5 +4,12 @@ describe('Write a Note Button Test', () => {
 
     cy.contains('Write a Note').click();
     cy.get('.modal-header > #note-writing-modal').contains('Write a Note').should('be.visible');
+    cy.get('.mr-2 > .radio-btn-label').contains('For myself').click();
+    cy.contains('button', 'Next').click();
+    cy.contains("Write a Note").should('be.visible'); 
+    cy.get('.modal-header > #note-writing-modal').contains("Write a Note").should('be.visible');
+    cy.get(':nth-child(1) > .radio-btn-wrapper > .radio-btn-label').contains("Conversation").click();
+    cy.get('span > .btn').contains('Write Note').click();
+    
   });
 });
